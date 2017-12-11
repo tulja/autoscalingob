fab0a50b0db8d41e7617b1a6f12a167cb5e2a6fb
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-11 22:21:58 +0530

    Update update_zabbix_agent.sh

--- update_zabbix_agent.sh
+++ update_zabbix_agent.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #resolving sudo: unable to resolve host <vnf-name>
-x=$(cat hostname)
-sudo sed -i "1s/.*/127.0.0.1 localhost $x/" hosts
+x=$(cat /etc/hostname)
+sudo sed -i "1s/.*/127.0.0.1 localhost $x/" /etc/hosts
 
 #auto discovery by zabbix
 #updating Server=localhost --> Server=10.0.0.5 for discovery by zabbix server as host
-line=$(sudo grep -n "Server=localhost" zabbix_agent.conf | sed 's/^\([0-9]\+\):.*$/\1/')
+line=$(sudo grep -n "Server=localhost" /etc/zabbix/zabbix_agent.conf | sed 's/^\([0-9]\+\):.*$/\1/')
 
 if ! [ -z "$line" ]
 then
-  sudo sed -i "${line}s/.*/Server=10.0.0.5/" zabbix_agent.conf
+  sudo sed -i "${line}s/.*/Server=10.0.0.5/" /etc/zabbix/zabbix_agent.conf
 fi
 
 #updating ServerActive=localhost:10051 --> ServerActive=10.0.0.5 for discovery by zabbix server as host
-line=$(sudo grep -n "ServerActive=localhost:10051$" zabbix_agent.conf | sed 's/^\([0-9]\+\):.*$/\1/')
+line=$(sudo grep -n "ServerActive=localhost:10051$" /etc/zabbix/zabbix_agent.conf | sed 's/^\([0-9]\+\):.*$/\1/')
 
 if ! [ -z "$line" ]
 then
-  sudo sed -i "${line}s/.*/ServerActive=10.0.0.5:10051\n/" zabbix_agent.conf
+  sudo sed -i "${line}s/.*/ServerActive=10.0.0.5:10051\n/" /etc/zabbix/zabbix_agent.conf
 fi
 
 sudo systemctl restart zabbix-agent.service 
