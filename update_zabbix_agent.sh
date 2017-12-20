4c95d0fde47e5040a68690a7583bbf3aa9cda7e0
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-20 20:32:09 +0530

    Update update_zabbix_agent.sh

--- update_zabbix_agent.sh
+++ update_zabbix_agent.sh
@@ -7,27 +7,10 @@ echo "changing hostname --> $x"
 
 #auto discovery by zabbix
 #updating Server=localhost --> Server=10.0.0.5 for discovery by zabbix server as host
-line=$(sudo grep -n "Server=localhost" /etc/zabbix/zabbix_agentd.conf | sed 's/^\([0-9]\+\):.*$/\1/')
-echo "changing Server=localhost --> $line"
 
-if ! [ -z "$line" ]
-then
-  echo "Inside 1st condition"
-  sudo sed -i "${line}s/.*/Server=10.0.0.5/" /etc/zabbix/zabbix_agentd.conf
-fi
-
-#updating ServerActive=localhost:10051 --> ServerActive=10.0.0.5 for discovery by zabbix server as host
-line=$(sudo grep -n "ServerActive=localhost:10051$" /etc/zabbix/zabbix_agentd.conf | sed 's/^\([0-9]\+\):.*$/\1/')
-echo "changing ServerActive=localhost:10051 --> $line"
-
-if ! [ -z "$line" ]
-then
-  echo "Inside 2nd condition"
-  sudo sed -i "${line}s/.*/ServerActive=10.0.0.5:10051\n/" /etc/zabbix/zabbix_agentd.conf
-fi
+echo "changing server IP"
+sudo sed -i "s+localhost+${zabbix_server_ip}+g" /etc/zabbix/zabbix_agentd.conf
 
 sudo systemctl restart zabbix-agent.service 
 cat /etc/zabbix/zabbix_agentd.conf
-cat /etc/hosts
-sudo systemctl status zabbix-agent.service 
 echo "restarted zabbix-agent.service"
