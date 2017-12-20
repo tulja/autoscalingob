f83f5a4f4fc240dbeaf4b86d01d34c65926a066c
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-20 20:52:31 +0530

    Update update_zabbix_agent.sh

--- update_zabbix_agent.sh
+++ update_zabbix_agent.sh
@@ -5,13 +5,23 @@ x=$(cat /etc/hostname)
 sudo sed -i "1s/.*/127.0.0.1 localhost $x/" /etc/hosts
 echo "changing hostname --> $x"
 
+echo " ****************  EXPORT -p **************************"
+export -p
+echo " ****************  EXPORT -p **************************"
+
+echo " ****************  env **************************"
+env
+echo " ****************  env **************************"
+
 #auto discovery by zabbix
 #updating Server=localhost --> Server=10.0.0.5 for discovery by zabbix server as host
 
-echo " **************** zabbix_server_ip --> ${zabbixserverip} **************************"
+echo " ****************  zabbix_server_ip **************************"
+$zabbixserverip
+echo " ****************  zabbix_server_ip **************************"
 
 echo "changing server IP"
-sudo sed -i "s+localhost+${zabbixserverip}+g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s+localhost+$zabbixserverip+g" /etc/zabbix/zabbix_agentd.conf
 
 sudo systemctl restart zabbix-agent.service 
 cat /etc/zabbix/zabbix_agentd.conf
