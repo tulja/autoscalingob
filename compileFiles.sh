115ae471a9e24fc3d92df5cf36ed330c39a8c769
gaurav1510 <cs16mtech11022@iith.ac.in>
2017-12-31 16:38:09 +0530

    Update compileFiles.sh

--- compileFiles.sh
+++ compileFiles.sh
@@ -2,8 +2,8 @@
 echo "Change dir to Scripts Folder " 
 cd /opt/openbaton/scripts/
 echo "Compiling the client and server programs"
-sudo g++ fst.cpp -o fst
-sudo g++ scnd.cpp -o scnd
-sudo g++ thrd.cpp -o thrd
+sudo g++ fst.cpp -lpthread -std=c++11 -o fst
+sudo g++ scnd.cpp -lpthread -std=c++11 -o scnd
+sudo g++ thrd.cpp -lpthread -std=c++11 -o thrd
 ls
 echo "Done!"
