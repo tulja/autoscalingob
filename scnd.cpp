66474216202932b71eb78406978b046f3517e766
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-31 22:37:52 +0530

    Update scnd.cpp

--- scnd.cpp
+++ scnd.cpp
@@ -14,63 +14,71 @@
 #include <mutex>
 #include <chrono>
 using namespace std;
-  char *cip="10.0.0.11";
-int flag=0;
+char *cip = "10.0.0.11";
+int flag = 0;
 
-int thread_server(){
-    int client, server,k;
-    int buffersize=(1+1)*sizeof(int);
+int thread_server() {
+    int client, server, k;
+    int buffersize = (1 + 1) * sizeof(int);
     ofstream fout;
-        
+
     int data[buffersize];
     struct sockaddr_in server_addr;
     socklen_t size;
     int portNum;
     /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
     /* --------------- socket() function ------------------*/
-    portNum=8089;
+    portNum = 8089;
     server = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server < 0) 
+    if (server < 0)
     {
-      cout << "server socket failed" << endl;
+        cout << "server socket failed" << endl;
     }
     else {
-      cout << "server socket success" << endl;
+        cout << "server socket success" << endl;
     }
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(portNum);
 
-    if ((bind(server, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
+    if ((bind(server, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0)
     {
-        cout<<"error in servers" << endl;
+        cout << "error in servers" << endl;
         return -1;
     }
-    else {
+    else
         cout << "bind success" << endl;
-    }
 
     size = sizeof(server_addr);
     listen(server, 10);
     int clientCount = 1;
-    while(1){
-        if ((client = accept(server,(struct sockaddr *)&server_addr,&size)) >= 0) {
-          flag=1;
-          cout<<"Received success"<<endl;
+    char* server_reply = new char[20000];
+    while (1) {
+
+        if ((client = accept(server, (struct sockaddr *)&server_addr, &size)) >= 0) {
+            while (1) {
+                if ( recv(client, server_reply , 20000 , 0) < 0)
+                {
+                    cout << "recv failed" << endl;
+                    break;
+                }
+                else {
+                    cout << "Received success" << endl;
+                }
+            }
         }
-       else {
-        cout << "Received failed" << endl;
-      }
+        else
+            cout << "Received failed" << endl;
     }
 }
 
 
 
-int main(int argc,const char* argv[]){
-    cout<<"Enter server and client ports - ";
-    
+int main(int argc, const char* argv[]) {
+    cout << "Enter server and client ports - ";
+
     thread t1(thread_server);
     t1.join();
     return 0;
