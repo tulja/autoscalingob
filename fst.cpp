1833ff075ea5ed5e6623b4b2921f607967454fdc
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-31 20:11:27 +0530

    Update fst.cpp

--- fst.cpp
+++ fst.cpp
@@ -14,100 +14,52 @@
 #include <mutex>
 #include <chrono>
 using namespace std;
-  char sip[100],cip[100];
+char sip[100],cip[100];
 int flag=1;
 
 int thread_client(){
   int portNum,remainder;
-  usleep(10000000);
     while(1){
         if(flag==1){
             flag=1;
             int client;
             bool isExit = false;
-            int bufsize = (10)*sizeof(int);
-            int buffer[bufsize];
+            int bufsize = 10;
             portNum=8089;
             struct sockaddr_in server_addr;
-       
-
+      
             l1: client = socket(AF_INET, SOCK_STREAM, 0);
             if (client < 0){
                 goto l1;
             }
-               server_addr.sin_family = AF_INET;
+            else {
+                cout << "socket creation failed" << endl;
+            }
+            server_addr.sin_family = AF_INET;
             server_addr.sin_port = htons(portNum);
        
             inet_pton(AF_INET, cip, &server_addr.sin_addr);
-            
             
            l2: 
            if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0) {
             while(1)
             {
+                const int* buffer = new int[bufsize];
                 send(client, buffer, bufsize, 0);
                 cout<<"Sent"<<endl;
-                close(client);
+                delete []buffer;
+                bufsize +=5;
             }
         }
             else 
             {
-            
+                cout << "socket connection failed" << endl;
                 goto l2;
-
             }
         }
     }
     return 0;
 }
-
-/*
-
-int thread_server(){
-    int client, server,k;
-    int buffersize=(1+1)*sizeof(int);
-    ofstream fout;
-        
-       int data[buffersize];
-       struct sockaddr_in server_addr;
-    socklen_t size;
-    int portNum;
-portNum=8089;
-    server = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server < 0) 
-    {
-         }
-
-    
-    
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(portNum);
-
-    if ((bind(server, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
-    {
-        cout<<"error in servers";
-        return -1;
-    }
-
-    
-    size = sizeof(server_addr);
-    listen(server, 10);
-    int clientCount = 1;
-    while(1){
-    if ((client = accept(server,(struct sockaddr *)&server_addr,&size)) >= 0) {
-        while(1)
-        {
-        flag=1;
-        cout<<"Received"<<endl;
-        }
-    }
-    
-    }
-}
-*/
 
 int main(int argc,const char* argv[]){
     cout<<"Enter server and client ports - ";
