4dcfb739d16c384fd449dee5f499fcec8ba30728
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-31 19:07:07 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,11 +3,6 @@ echo "Change the Directory to /opt/openbaton/scripts"
 cd /opt/openbaton/scripts/
 echo "Client has started and is trying to connect to Server at port number 8089"
 #wait for 10 seconds for server to become active 
-echo "Server IP is " 
-echo $server_private
-echo "Server Hostname is "
-echo $server_hostname 
-echo "second private is "
 
 # ./client $server_private > client_output.txt
 screen -d -m -S my ./scnd
