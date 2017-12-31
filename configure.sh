0dc86f4be3ed3888a5c0482680e115d8654e569b
Harsh Agarwal <cs15btech11019@iith.ac.in>
2017-12-31 19:07:53 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Change the Directory to /opt/openbaton/scripts"
 cd /opt/openbaton/scripts/
-echo "Client has started and is trying to connect to Server at port number 8089"
+echo "second VNF start no depenedency"
 #wait for 10 seconds for server to become active 
 
 # ./client $server_private > client_output.txt
