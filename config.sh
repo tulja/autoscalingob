187f49d7d049ebe41eb21401efa4cd9a44d13e63
tulja <btuljavamshikiran@gmail.com>
2018-01-06 18:51:57 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,3 +2,6 @@
 
 cd /opt/openbaton/scripts/
 sudo cp zabbix_agentd.conf /etc/zabbix/
+sudo systemctl restart zabbix-agent.service 
+#cat /etc/zabbix/zabbix_agentd.conf
+echo "restarted zabbix-agent.service"
