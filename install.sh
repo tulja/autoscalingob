e6c74354145718033a7bc58b498a0894e631cdfc
tulja <btuljavamshikiran@gmail.com>
2018-01-12 23:42:46 +0530

    updated to Non interactive mode

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update && sudo apt-get install -y iperf screen
 sudo apt-get install iperf3 
-sudo apt-get install snmpd
+DEBIAN_FRONTEND=noninteractive  sudo apt-get install snmpd -y
 
