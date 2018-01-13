6e6395195402af29b40acfbe541ab9767e94921b
tulja <btuljavamshikiran@gmail.com>
2018-01-13 09:45:22 +0530

    stopped zabbix agent

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo systemctl stop zabbix-agent.service
 sudo apt-get update && sudo apt-get install -y iperf screen
 sudo apt-get install iperf3 
 DEBIAN_FRONTEND=noninteractive  sudo apt-get install snmpd -y
