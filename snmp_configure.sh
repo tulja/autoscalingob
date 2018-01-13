bb746b8824daee04c60fcb74656dc66629f32bd4
tulja <btuljavamshikiran@gmail.com>
2018-01-13 10:55:45 +0530

    Update snmp_configure.sh

--- snmp_configure.sh
+++ snmp_configure.sh
@@ -11,3 +11,8 @@ echo "syscontact  cs16mtech11020@iith.ac.in" >> snmpd.conf
 ############
 
 sudo /etc/init.d/snmpd restart
+cd /opt/openbaton/scripts/
+sudo cp zabbix_agentd.conf /etc/zabbix/
+sudo systemctl start zabbix-agent.service 
+#cat /etc/zabbix/zabbix_agentd.conf
+echo "started zabbix-agent.service"
