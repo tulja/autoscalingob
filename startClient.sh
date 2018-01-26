d316460b0b52982e922ed497568b519cc05b08d0
tulja <btuljavamshikiran@gmail.com>
2018-01-26 12:46:02 +0530

    Delete startClient.sh

--- startClient.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Change the Directory to /opt/openbaton/scripts"
-cd /opt/openbaton/scripts/
-echo "Client has started and is trying to connect to Server at port number 8089"
-#wait for 10 seconds for server to become active 
-sleep 10s 
-screen -d -m -S client ./client $server_private 
