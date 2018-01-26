a56ff370489c64c13d7dbcceb827a1f6d9fff576
tulja <btuljavamshikiran@gmail.com>
2018-01-26 12:46:07 +0530

    Delete startServer.sh

--- startServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Change the Directory to /opt/openbaton/scripts"
-cd /opt/openbaton/scripts/
-echo "Server has started and is listening at port number 8089"
-#run the server
-screen -d -m -S server ./server
