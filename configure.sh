dba39b346bff4b6aefc39b634cb8340d38f054c2
tulja <btuljavamshikiran@gmail.com>
2018-01-26 12:50:13 +0530

    Delete configure.sh

--- configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Change the Directory to /opt/openbaton/scripts"
-cd /opt/openbaton/scripts/
-echo "second VNF start no depenedency"
-#wait for 10 seconds for server to become active 
-
-# ./client $server_private > client_output.txt
-screen -d -m -S my ./scnd
